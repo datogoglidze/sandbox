259977fa936e57c60c8918dcc13f83931cc8a070
Schwifter <d.goglidze@sangu.edu.ge>
2024-01-25 01:44:08 +0400

    Rename

--- card.py
+++ card.py
@@ -10,28 +10,20 @@ class Card:
         return len(self.number)
 
     def read_card(self) -> str:
-        if (
-            self.length_check([15])
-            and self.start_check(["34", "37"])
-            and self.validity_check()
-        ):
+        if self.length_is([15]) and self.starts_with(["34", "37"]) and self.is_valid():
             return "AMEX\n"
-        elif (
-            self.length_check([13, 16])
-            and self.start_check(["4"])
-            and self.validity_check()
-        ):
+        elif self.length_is([13, 16]) and self.starts_with(["4"]) and self.is_valid():
             return "VISA\n"
         elif (
-            self.length_check([16])
-            and self.start_check(["51", "52", "53", "54", "55"])
-            and self.validity_check()
+            self.length_is([16])
+            and self.starts_with(["51", "52", "53", "54", "55"])
+            and self.is_valid()
         ):
             return "MASTERCARD\n"
         else:
             return "INVALID\n"
 
-    def validity_check(self) -> bool:
+    def is_valid(self) -> bool:
         results = []
 
         for digit in range(self.length - 2, -1, -2):
@@ -48,13 +40,13 @@ class Card:
 
         return False
 
-    def length_check(self, expected_length: list[int]) -> bool:
+    def length_is(self, expected_length: list[int]) -> bool:
         if self.length in expected_length:
             return True
 
         return False
 
-    def start_check(self, digits: list[str]) -> bool:
+    def starts_with(self, digits: list[str]) -> bool:
         if self.number[: len(digits[0])] in digits:
             return True
 
@@ -63,17 +55,17 @@ class Card:
 
 def test_amex() -> None:
     card = Card("378282246310005")
-    assert card.validity_check()
+    assert card.is_valid()
     assert card.read_card() == "AMEX\n"
 
 
 def test_mastercard() -> None:
     card = Card("5555555555554444")
-    assert card.validity_check()
+    assert card.is_valid()
     assert card.read_card() == "MASTERCARD\n"
 
 
 def test_invalid() -> None:
     card = Card("1234567890")
-    assert not card.validity_check()
+    assert not card.is_valid()
     assert card.read_card() == "INVALID\n"
