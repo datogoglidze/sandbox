871eab341a042ae6f953140816ff10521fc2490c
Schwifter <d.goglidze@sangu.edu.ge>
2024-01-26 00:07:13 +0400

    Extract class

--- readability.py
+++ readability.py
@@ -1,7 +1,26 @@
-def main(text: str) -> str:
-    letters = count_letters(text)
-    words = count_words(text)
-    sentences = count_sentences(text)
+from __future__ import annotations
+
+from dataclasses import dataclass
+
+
+@dataclass
+class Text:
+    text: str
+
+    def count_letters(self) -> int:
+        return sum([1 for i in range(len(self.text)) if self.text[i].isalpha()])
+
+    def count_words(self) -> int:
+        return self.text.count(" ") + 1
+
+    def count_sentences(self) -> int:
+        return self.text.count(".") + self.text.count("?") + self.text.count("!")
+
+
+def main(text: Text) -> str:
+    letters = text.count_letters()
+    words = text.count_words()
+    sentences = text.count_sentences()
 
     grade = round(
         0.0588 * (letters / words * 100.00)
@@ -17,33 +36,23 @@ def main(text: str) -> str:
         return f"Grade {grade}"
 
 
-def count_letters(text: str) -> int:
-    return sum([1 for i in range(len(text)) if text[i].isalpha()])
-
-
-def count_words(text: str) -> int:
-    return text.count(" ") + 1
-
-
-def count_sentences(text: str) -> int:
-    return text.count(".") + text.count("?") + text.count("!")
-
-
 def test_before_grade_one():
-    text_ = "Text: One fish. Two fish. Red fish. Blue fish."
-    assert count_letters(text_) == 33
-    assert count_words(text_) == 9
-    assert count_sentences(text_) == 4
+    text_ = Text("Text: One fish. Two fish. Red fish. Blue fish.")
+    assert text_.count_letters() == 33
+    assert text_.count_words() == 9
+    assert text_.count_sentences() == 4
     assert main(text_) == "Before Grade 1"
 
 
 def test_grade_three():
-    text_ = "Congratulations! Today is your day. You're off to Great Places! You're off and away!"
+    text_ = Text(
+        "Congratulations! Today is your day. You're off to Great Places! You're off and away!"
+    )
     assert main(text_) == "Grade 3"
 
 
 def test_grade_five():
-    text_ = (
+    text_ = Text(
         "Harry Potter was a highly unusual boy in many ways. For one thing, he hated the summer holidays more "
         "than any other time of year. For another, he really wanted to do his homework, but was forced to do it "
         "in secret, in the dead of the night. And he also happened to be a wizard."
@@ -52,7 +61,7 @@ def test_grade_five():
 
 
 def test_grade_eight():
-    text_ = (
+    text_ = Text(
         "When he was nearly thirteen, my brother Jem got his arm badly broken at the elbow. When it healed, "
         "and Jem's fears of never being able to play football were assuaged, he was seldom self-conscious about "
         "his injury. His left arm was somewhat shorter than his right; when he stood or walked, the back of his "
@@ -62,7 +71,7 @@ def test_grade_eight():
 
 
 def test_grade_max():
-    text_ = (
+    text_ = Text(
         "A large class of computational problems involve the determination of properties of graphs, digraphs, "
         "integers, arrays of integers, finite families of finite sets, boolean formulas and elements of other "
         "countable domains."
